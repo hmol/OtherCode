af4f8b236d9f156da9bccd1f2347cf7c74b4316f
Henrik Molnes <i51@spk.no>
2016-02-15 17:18:13 +0100

    added chocolatey script

---
+++ chocolatey script/Readme.md5
@@ -0,0 +1,1 @@
+Use chocolatey (https://chocolatey.org/) to easy install most of what I need in my dev environment.
---
+++ chocolatey script/run_on_new_dev_machine.ps1
@@ -0,0 +1,2 @@
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install rdcman git SourceTree adobereader dotTrace dotPeek winmerge fiddler GoogleChrome Firefox notepadplusplus vlc urlrewrite 7zip katmouse lockhunter puretext kdiff3 linqpad
