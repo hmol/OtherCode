de307ce291b7a39dcbf055688d5c9cd15a2f56f2
henrik molnes <molnes@gmail.com>
2016-09-02 15:00:27 +0200

    Update run_on_new_dev_machine.ps1

--- chocolatey script/run_on_new_dev_machine.ps1
+++ chocolatey script/run_on_new_dev_machine.ps1
@@ -1,2 +1,2 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install rdcman git SourceTree adobereader dotTrace dotPeek winmerge fiddler GoogleChrome Firefox notepadplusplus vlc urlrewrite 7zip katmouse lockhunter puretext kdiff3 linqpad
+choco install dropbox 1password GoogleChrome visualstudio2015professional dotnetcore-runtime rdcman git SourceTree adobereader dotTrace dotPeek winmerge fiddler Firefox notepadplusplus vlc 7zip kdiff3 linqpad lastpass
